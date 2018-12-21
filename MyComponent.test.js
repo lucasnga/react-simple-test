d256f6d0d8095c16d0c24b2d08fb90aaa60adf15
Łukasz Anuszkiewicz <odysey84@o2.pl>
2018-12-21 18:30:24 +0100

    Add test for state changing methods

--- MyComponent.test.js
+++ MyComponent.test.js
@@ -16,4 +16,25 @@ describe('MyComponent should not fail', () => {
 	it('MyComponent renders', () => {
 		ReactDOM.render(React.createElement(MyComponent, null), document.createElement('div'));
 	});
+
+	it('You can\'t invoke methods on not mounted component', () => {
+		const MyNewComponent = new MyComponent();
+		console.error = jest.fn()
+		MyNewComponent.incHandler()
+		expect(console.error).toHaveBeenCalled();
+	});
+
+	it('Increment handle should increment value in state', () => {
+		const MyMountedComponent = ReactDOM.render(React.createElement(MyComponent, null), document.createElement('div'));
+		MyMountedComponent.state.value = 10;
+		MyMountedComponent.incHandler();
+		expect(MyMountedComponent.state.value).toEqual(11);
+	});
+
+	it('Decrement handle should decrement value in state', () => {
+		const MyMountedComponent = ReactDOM.render(React.createElement(MyComponent, null), document.createElement('div'));
+		MyMountedComponent.state.value = 10;
+		MyMountedComponent.decHandler();
+		expect(MyMountedComponent.state.value).toEqual(9);
+	});
 });
